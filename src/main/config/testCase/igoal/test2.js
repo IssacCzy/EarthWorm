715a9240fa6c3d6bcd5e9fd127b0c2e20ffcc17d
cuizhanyou <cuizhanyou@gomeplus.com>
2016-07-29 17:05:57 +0800

    删除无用文件

--- src/main/config/testCase/igoal/test2.js
+++
@@ -1,9 +0,0 @@
-var BrowserAction = Java.type("com.issac.earthworm.BrowserAction");
-var browser = new BrowserAction("igoal","test2");
-
-browser.scenario("百度搜索",1);
-browser.open("http://www.baidu.com");
-browser.type("//*[@id=\"kw\"]","自动化测试 Selenium");
-browser.click("//*[@id=\"su\"]");
-
-browser.end();
